63b3cd2d6aa568fbcf5010bb6968c259104c7cdb
LordGhost001 <ghostsmith001@gmail.com>
2023-08-13 17:22:16 -0700

    print size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description: prints size of char, int, long int, long long int and float
- * Return: Always 0 (Success)
- */
-
+*main - Entry point
+*Descripton: "prints the size of various files"
+*Return: Always 0 (Success)
+*/
 int main(void)
-{
-	char v;
-	int w;
-	long int x;
-	long long int y;
-	float z;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(v));
-	printf("Size of a int: %zu byte(s)\n", sizeof(w));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(x));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(y));
-	printf("Size of a float: %zu byte(s)\n", sizeof(z));
-	return (0);
+{
+int v;
+long int w;
+long long int x;
+char y;
+float z;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(y));
+printf("Size of an int: %zu byte(s)\n", sizeof(v));
+printf("Size of a long int: %zu byte(s)\n", sizeof(w));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(x));
+printf("Size of a float: %zu byte(s)\n", sizeof(z));
+return (0);
+
 }
+
