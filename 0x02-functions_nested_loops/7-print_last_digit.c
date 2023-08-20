03299c613030c0aedf4b8e662ff882333b639707
LordGhost001 <ghostsmith001@gmail.com>
2023-08-20 01:23:57 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,15 @@ int print_last_digit(int n)
 {
 	int lastdigit;
 
+	if (n < 0)
+		n = -n;
+
 	lastdigit = n % 10;
+
+	if (lastdigit < 0)
+		lastdigit = -lastdigit;
+
 	_putchar(lastdigit);
+
 	return (lastdigit);
 }
