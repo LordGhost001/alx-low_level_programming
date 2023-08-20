cd46e7c9da09298a6de76da2e5b3994ae5b64585
LordGhost001 <ghostsmith001@gmail.com>
2023-08-20 02:32:11 -0700

    add 2 nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - prints sum of 2 nums
+* @a: first arg to be added
+* @b: second arg to be added
+* Return: sum of both args
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
