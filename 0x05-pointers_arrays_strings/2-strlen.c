61486f80fb40c14094482c4f60d6aaa86cbc2aad
LordGhost001 <ghostsmith001@gmail.com>
2023-08-27 23:27:05 -0700

    calc the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strelen - gets the length of a string
+* @*s: arg to calc length of its string
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
